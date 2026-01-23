713465a7e713fe83ccc512d0127665ba540331c1
Piotr Mankowski <pmanko@uw.edu>
2026-01-23 05:02:01 +0000

    fix(M4): Export SerialHandler and FileHandler in protocols package

--- protocols/__init__.py
+++ protocols/__init__.py
@@ -7,5 +7,7 @@ Reference: specs/011-madagascar-analyzer-integration/plan.md.
 from .base_handler import BaseHandler
 from .astm_handler import ASTMHandler, generate_astm_message
 from .hl7_handler import HL7Handler
+from .serial_handler import SerialHandler
+from .file_handler import FileHandler
 
-__all__ = ["BaseHandler", "ASTMHandler", "HL7Handler", "generate_astm_message"]
+__all__ = ["BaseHandler", "ASTMHandler", "HL7Handler", "SerialHandler", "FileHandler", "generate_astm_message"]
